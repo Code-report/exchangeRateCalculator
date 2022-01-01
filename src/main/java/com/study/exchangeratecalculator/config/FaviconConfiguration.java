9e9851eabbb4e06e7c0d6316efe5b3bc20fb3d91
cwpark <cwpark@tesna.co.kr>
2022-01-01 15:56:43 +0900

    Language 선택 기능 구현

---
+++ src/main/java/com/study/exchangeratecalculator/config/FaviconConfiguration.java
@@ -0,0 +1,2 @@
+package com.study.exchangeratecalculator.config;public class FaviconConfiguration {
+}
---
+++ src/main/java/com/study/exchangeratecalculator/config/LocaleConfiguration.java
@@ -0,0 +1,46 @@
+package com.study.exchangeratecalculator.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.KOREA);
+        cookieLocaleResolver.setCookieName("APPLICATION_LOCALE");
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("locale");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+}
